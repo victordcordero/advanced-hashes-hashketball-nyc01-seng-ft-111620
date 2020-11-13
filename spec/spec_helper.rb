f382a130e8970004bcd443f791a243decdf7e8bc
Victor Cordero <victordcordero@gmail.com>
2020-11-13 22:09:22 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -45,7 +45,8 @@ end
 
 def player_stats(player_name)
   game_hash.each do |home_away, keys|
-    if keys[:player_name] == player_name
+    keys[:players].each do |player|
+    if player[:player_name] == player_name
       return keys[:player_name]
     end
   end
